48da6409695f06d1fe0e32ff207d73147ec5a055
Vignesh100799 <baluvignesh422@gmail.com>
2023-11-24 17:36:51 +0530

    check

--- src/App.js
+++ src/App.js
@@ -1,37 +1,27 @@
 import './App.css';
-import './components/Product'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap";
-import './components/Home'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { UserProvider } from "./context/Usercontext";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Allproducts from "./components/Allproducts";
 import Cartpage from "./components/Cartpage";
-import Navbar from "./components/Navbar";
 import Popular from "./components/Popular";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { UserProvider } from "./context/Usercontext";
 
 function App() {
-
-
-
   return (
-    
-        <BrowserRouter>
-          <UserProvider>
-            <Navbar />
-            <Routes>
-             
-                  <Route path="/" element={<Allproducts />} />
-                
-              <Route path="/popular" element={<Popular />} />
-              <Route path='/cart' element={<Cartpage />} />
-            </Routes>
-            <Footer />
-          </UserProvider>
-        </BrowserRouter>
-    
+    <BrowserRouter>
+      <UserProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Allproducts />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/cart" element={<Cartpage />} />
+        </Routes>
+        <Footer />
+      </UserProvider>
+    </BrowserRouter>
   );
 }
 
